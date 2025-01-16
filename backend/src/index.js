999fb1bf65832cd055c93f8e2d4f37c26fe83a05
anurag-chandra9 <anuragkrstm01@gmail.com>
2025-01-16 21:25:32 +0530

    production

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from "cors";
+import path from 'path';
+
 import {app,server} from './lib/socket.js';
 dotenv.config();
 
@@ -11,6 +13,7 @@ import messageRoutes from './routes/message.route.js';
 import { connectDB } from './lib/db.js';
 
 const PORT=process.env.PORT || 3001;
+const __dirname = path.resolve();
 app.use(express.json({limit: '50mb'}));
 app.use(cookieParser());
 
@@ -25,8 +28,13 @@ app.use("/api/messages",messageRoutes);
 
 // // For URL-encoded payloads
 // app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../frontend',"dist","index.html"));
+        })
+}
 
 server.listen(PORT, () => {
     console.log('Server is running on port:'+PORT);
